a7682e6187fe623fd97d2222e5052dc8b9ec2505
kailash8799 <kailashrajput8799@gmail.com>
2023-05-12 20:24:47 +0530

    Start new project with react native

---
+++ src/Top.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Top = () => {
+  return (
+    <View>
+        
+    </View>
+  )
+}
+
+export default Top
+
+const styles = StyleSheet.create({})
